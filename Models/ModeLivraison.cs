2751c9183e0f7fa774cd70f46e76abf2c5abf65b
tafraouti-sanae01 <tafraouti.sanae1@gmail.com>
2025-12-28 00:36:35 +0100

    Amélioration livraison et checkout: modèles, contrôleurs, vues et scripts SQL

--- Models/ModeLivraison.cs
+++ Models/ModeLivraison.cs
@@ -7,8 +7,7 @@ namespace E_Commerce_Cooperatives.Models
         public int ModeLivraisonId { get; set; }
         public string Nom { get; set; }
         public string Description { get; set; }
-        public decimal Tarif { get; set; }
-        public string DelaiEstime { get; set; }
+        public decimal Tarif { get; set; } // Prix de base du mode (33 MAD pour Standard, 60 MAD pour Express)
         public bool EstActif { get; set; }
         public DateTime DateCreation { get; set; }
     }
